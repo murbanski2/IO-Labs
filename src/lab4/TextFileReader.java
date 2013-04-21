224473bbc3c8500fc75f1186e48909346975aa82
murbanski@my.wctc.edu <>
2013-04-21 15:06:32 -0500

    TextFileReader in process.

--- src/lab4/TextFileReader.java
+++ src/lab4/TextFileReader.java
@@ -1,5 +1,11 @@
 package lab4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Mark Urbanski
@@ -13,8 +19,33 @@ public class TextFileReader {
         this.filePath = filePath;
     }
     
-    public void readFile() {
-    
+    public List<String> readFile() throws IOException{
+        List<String> stringsFromFile = new ArrayList<String>();
+        BufferedReader inFile = null;
+        
+        try{
+            inFile = new BufferedReader(new FileReader(filePath));
+            String line = inFile.readLine();
+            while (line != null) {
+                String str[] = line.split("_");
+                if (str.length >=8) {
+                    setContact(str);
+                    System.out.println(contact.toString());
+                }
+                else{
+                    System.out.println("Input line did not have enough fields");
+                }
+                //outputRecord(str);
+                line = in.readLine();  // strips out any carriage return chars
+            }
+        }
+        catch(IOException ioe) {
+            throw ioe;
+        }
+        finally {
+            
+        }   
+        return stringsFromFile;
     }
 
     public String getFilePath() {
