68b7d6a9852b4b0ed0d22a4b86a7b0a9b8280e5a
murbanski@my.wctc.edu <>
2013-04-21 14:42:50 -0500

    Starting the TextFileReader

--- src/lab4/TextFileReader.java
+++ src/lab4/TextFileReader.java
@@ -5,5 +5,27 @@ package lab4;
  * @author Mark Urbanski
  */
 public class TextFileReader {
+    private String filePath;
+
+
+    
+    public TextFileReader(String filePath) {
+        this.filePath = filePath;
+    }
+    
+    public void readFile() {
+    
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+
+    
 
 }
