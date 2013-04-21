68e224b6840f7b678ceedc4fc255c524ee6a6cd3
murbanski@my.wctc.edu <>
2013-04-21 14:31:48 -0500

    Startup and TextFileReader for lab4, the FileReader

---
+++ src/lab4/Startup.java
@@ -0,0 +1,11 @@
+package lab4;
+
+/**
+ *
+ * @author Mark Urbanski
+ */
+public class Startup {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/lab4/TextFileReader.java
@@ -0,0 +1,9 @@
+package lab4;
+
+/**
+ *
+ * @author Mark Urbanski
+ */
+public class TextFileReader {
+
+}
