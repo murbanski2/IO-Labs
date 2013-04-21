0d9e216a9d8e13e451c537bd6ddddc77e43c0a10
murbanski@my.wctc.edu <>
2013-04-21 15:41:43 -0500

    TextFileReader works by returning strings.  No parsing yet.

--- src/lab4/TextFileReader.java
+++ src/lab4/TextFileReader.java
@@ -1,6 +1,7 @@
 package lab4;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,23 +28,17 @@ public class TextFileReader {
             inFile = new BufferedReader(new FileReader(filePath));
             String line = inFile.readLine();
             while (line != null) {
-                String str[] = line.split("_");
-                if (str.length >=8) {
-                    setContact(str);
-                    System.out.println(contact.toString());
-                }
-                else{
-                    System.out.println("Input line did not have enough fields");
-                }
-                //outputRecord(str);
-                line = in.readLine();  // strips out any carriage return chars
+                stringsFromFile.add(line);                
+                line = inFile.readLine();  // strips out any carriage return chars
             }
         }
         catch(IOException ioe) {
             throw ioe;
         }
         finally {
-            
+            if(inFile != null) {
+                inFile.close();
+            }
         }   
         return stringsFromFile;
     }
@@ -56,7 +51,19 @@ public class TextFileReader {
         this.filePath = filePath;
     }
 
-
+    
+    //Testing code only
+    public static void main(String[] args) throws IOException {
+        TextFileReader reader = 
+                new TextFileReader("src" + File.separatorChar 
+                + "test_comma_only.csv");
+        List<String> dataStrings = reader.readFile();
+        System.out.println(dataStrings);
+        for(String s : dataStrings) {
+            System.out.println(s);
+        }
+        
+    }
     
 
 }
