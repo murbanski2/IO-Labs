be96b416ead17e3bf5bb600964324faf153944c4
murbanski@my.wctc.edu <>
2013-04-21 21:35:09 -0500

    Started on TextFileWriter

--- src/lab4/TextFileWriter.java
+++ src/lab4/TextFileWriter.java
@@ -1,6 +1,10 @@
 package lab4;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -10,15 +14,42 @@ import java.util.List;
 class TextFileWriter {
     private String filePath;
     private List<String> stringsToSave;
+    private boolean append;
         
-    public TextFileWriter(String filePath, List<String> stringsToSave) {
+    public TextFileWriter(String filePath, List<String> stringsToSave,
+            boolean append) {
         this.filePath = filePath;
         this.stringsToSave = stringsToSave;
+        this.append = append;
     }
     
+    //FileWriter wrapped in a BufferedWriter wrapped in a PrintWriter
+    //When append = false, overwrite
     public void writeFile() throws IOException {
+        File data = null;
+        PrintWriter out = null;
         
-        
+        try {
+            data = new File(filePath);
+            out = new PrintWriter(
+                  new BufferedWriter(
+                  new FileWriter(data, append)) );
+            for(String s : stringsToSave) {
+                out.println();
+            }
+        }
+        catch(IOException ioe) {
+            throw ioe;
+        }
+        finally{
+            if(out != null){
+                out.close();
+            }
+        }
+    }
+    
+    //For testing only
+    public static void main(String[] args) {
         
     }
 }
